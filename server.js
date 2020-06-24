16b796996d3311447c593e2db8526db7f3205d90
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-06-24 16:11:13 -0500

    Debugging the app server

--- server.js
+++ server.js
@@ -8,15 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/math', (req, res) => {
-    res.render('pages/results', handleMath(req));
-  })
-  .get('/math_service', (req, res) => {
-    const obj = JSON.stringify(handleMath(req));
-    res.send(obj);
-  })
-  .get('/getRate', (req, res) => {
-    res.render('pages/postalrate', calculatePostage(req))
-  })
   .get('/api/customers', testConnect)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
